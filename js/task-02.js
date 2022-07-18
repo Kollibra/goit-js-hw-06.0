c2ef95478c3ea30dde2829dc7f5a8784d41ea92f
Iryna Kovalova <kovra777@gmail.com>
2022-07-18 16:19:38 +0300

    Сделала Task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,18 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listEl = document.querySelector('#ingredients')
+// console.log(listEl); получила ссылку на список
+const itemEl = ingredients.map(ingredient => { 
+  const itemsEl = document.createElement('li')
+  itemsEl.classList.add(`item`);
+  itemsEl.textContent = ingredient;
+
+
+  return itemsEl;
+});
+
+// console.log(itemEl); 
+listEl.append(...itemEl);
+
