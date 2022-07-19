f28ac6d9d06d359ed4093763c8433a5b1b84f14f
Iryna Kovalova <kovra777@gmail.com>
2022-07-19 19:04:40 +0300

    Решение Task 3

--- js/task-03.js
+++ js/task-03.js
@@ -14,3 +14,18 @@ const images = [
 ];
 
 
+
+
+const galleryEl = document.querySelector('.gallery');
+
+const createEl = images.map(({url, alt}) =>
+     `<li><img src = "${url}" alt = "${alt} 
+     width = "200" height = "200" ></li>`)
+     .join('');
+
+     
+galleryEl.style.display = 'flex';
+galleryEl.style.justifyContent = 'space-evenly';
+
+
+galleryEl.insertAdjacentHTML('afterbegin', createEl);
