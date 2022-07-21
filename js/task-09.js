2f4cbe899896cea4ac70be80e3bcd37c4f55481e
Iryna Kovalova <kovra777@gmail.com>
2022-07-21 19:49:37 +0300

    Решение Task9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+
+const button = document.querySelector('.change-color');
+const color = document.querySelector('.color');
+
+button.addEventListener('click', onButtonClick);
+
+function onButtonClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  color.textContent = document.body.style.backgroundColor;
+};
+
