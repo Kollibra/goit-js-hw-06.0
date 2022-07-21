68f8ecc899ea8a71d55b074bf9f23011081ce925
Iryna Kovalova <kovra777@gmail.com>
2022-07-21 19:18:13 +0300

    Решение Task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,24 @@
 
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+  const {
+    elements: { email, password } 
+  } = event.currentTarget;
+
+
+ if (email.value === "" || password.value === "") {
+    alert('Please fill in all the fields!') 
+}
+
+ const userForm = { email: email.value, Password: password.value }
+    console.log(userForm)
+    event.currentTarget.reset()
+}   
+    
+    
+    
+
